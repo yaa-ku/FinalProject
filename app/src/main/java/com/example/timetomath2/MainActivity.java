617b888355b8bb0adba5c378adf5adbf69258562
yanakuz <yana.yana.kuznecova666@gmail.com>
2019-05-06 03:36:59 +1000

    Second step
    New design

--- app/src/main/java/com/example/timetomath2/MainActivity.java
+++ app/src/main/java/com/example/timetomath2/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.timetomath2;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.Placeholder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout layout;
+    public Placeholder placeholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+        placeholder = findViewById(R.id.placeholder);
+
+    }
+    public void swapView(View v){
+        TransitionManager.beginDelayedTransition(layout);
+placeholder.setContentId(v.getId());
     }
 }
