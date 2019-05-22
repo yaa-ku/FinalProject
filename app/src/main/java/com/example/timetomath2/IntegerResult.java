090494ab28f75822c0af7d30bc9e7d5ff4de3d12
yanakuz <yana.yana.kuznecova666@gmail.com>
2019-05-22 15:57:10 +1000

    new2
    dsdsd

--- app/src/main/java/com/example/timetomath2/IntegerResult.java
+++ app/src/main/java/com/example/timetomath2/IntegerResult.java
@@ -1,7 +1,9 @@
 package com.example.timetomath2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class IntegerResult extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class IntegerResult extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_integer_result);
+    }
+
+    public void onIntInfo(View view) {
+        Intent intent = new Intent(IntegerResult.this, IntegerInfo.class);
+        startActivity(intent);
+    }
+
+    public void onMenuInt(View view) {
+        Intent intent = new Intent(IntegerResult.this, MainActivity.class);
+        startActivity(intent);
     }
 }
